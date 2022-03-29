079731cdd7909e9a5bd6d29b589e52969768952a
SinaBrauch <sina.brauch@gmail.com>
2022-03-29 14:22:40 +0200

    meme generator form completed

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,7 +2,6 @@ import memesData from '../memesData.js'
 import { useState } from 'react';
 
 export default function Meme() {
-
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
@@ -21,26 +20,51 @@ export default function Meme() {
       randomImg: url
     }));
   }
-
+  
+  const [className, setClassName] = useState('form-button')
+  
+// is active ? True / False ?
   //Button Style change
   function handleOnmouseOver() {
-    let button = document.querySelector('.form-button');
-    button.classList.toggle("mouseOver");
+    setClassName('form-button menu-active');
   }
   function handleOnmouseLeave() {
-    let button = document.querySelector('.form-button');
-    button.classList.toggle("mouseOver");
+    setClassName('form-button');
+  }
+
+  function handleChange(event) {
+    const {name, type, value} = event.target
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
   }
 
   return (
     <main>
       <article className="form">
-        <input type="text" placeholder="Top text"/>
-        <input type="text" placeholder="Button text"/>
-        <button className="form-button" onClick={getMemeImage} onMouseOver={handleOnmouseOver} onMouseLeave={handleOnmouseLeave}>Get a new meme image 🖼</button>
-        <figure>
+        <input 
+          type="text" 
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input 
+          type="text" 
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button className={className} onClick={getMemeImage} onMouseOver={handleOnmouseOver} onMouseLeave={handleOnmouseLeave}>Get a new meme image 🖼</button>
+      </article>
+      <article className="meme">
+      <figure>
           <img src={meme.randomImg} className="meme-img"/>
         </figure>
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
       </article>
     </main>
   )
